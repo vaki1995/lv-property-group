a58a8108fb2f58bec20989d1e61606ac9fa0f35a
kiennguyen1995 <kien.nguyen@dirox.net>
2024-01-11 10:53:01 +0700

    Update form

--- custom.js
+++ custom.js
@@ -1,4 +1,5 @@
 $( document ).ready(function() {
+    let modal = bootstrap.Modal.getOrCreateInstance($("#modalFormResult")) // Returns a Bootstrap modal instance
     $("#contact-section .dropdown-item").click(function(){
         var text = $(this).text();
         $("#contact-section .dropdown-toggle").text(text);
@@ -16,5 +17,26 @@ $( document ).ready(function() {
         $(this).addClass('active');
         $(".menu-area a").removeClass('active');
         $(".menu-area ." + item).addClass('active');
+    });
+    $("#form-contact").submit(function(e) {
+        e.preventDefault();
+        if($("#exampleFormName").val() == ""){
+            alert("Please enter your name");
+        }else if($("#exampleFormEmail").val() == ""){
+            alert("Please enter your email");
+        }else if($("#exampleFormPhone").val() == ""){
+            alert("Please enter your phone");
+        }else if($("#exampleFormControlTextarea").val() == ""){
+            alert("Please enter your message");
+        }else{
+            modal.show();
+        }
+    });
+    $("#btnConfirm").click(function(){
+        modal.hide();
+        $("#exampleFormName").val("");
+        $("#exampleFormEmail").val("");
+        $("#exampleFormPhone").val("");
+        $("#exampleFormControlTextarea").val("");
     });
 });
